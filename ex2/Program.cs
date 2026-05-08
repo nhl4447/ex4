4cc69c1372ea7c7a32e0c04926b0c2b3784ecfdf
Lam Nguyen <pronhl4447@gmail.com>
2026-05-08 23:36:34 +0700

    Add project files.

---
+++ ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            Console.WriteLine("y\tx");
+
+            for (int y = -5; y <= 5; y++)
+            {
+                x = y * y + 2 * y + 1;
+
+                Console.WriteLine(y + "\t" + x);
+            }
+        }
+    }
+
+}
